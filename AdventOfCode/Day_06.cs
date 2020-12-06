49ea2e26ce71a7162b61a916e64cdf9bc5c3cbd9
Dan Rogers <drogers@trakglobal.co.uk>
2020-12-06 11:13:51 +0000

    Day 06

---
+++ AdventOfCode/Day_06.cs
@@ -0,0 +1,58 @@
+﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public class Day_06 : BaseDay
+    {
+        private readonly List<List<List<char>>> _groups;
+
+        public Day_06()
+        {
+            _groups = new List<List<List<char>>>();
+
+            var group = new List<List<char>>();
+
+            foreach (var line in File.ReadAllLines(InputFilePath))
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    _groups.Add(group);
+                    group = new List<List<char>>();
+                    continue;
+                }
+
+                group.Add(line.Distinct().ToList());
+            }
+
+            _groups.Add(group);
+        }
+
+        public override string Solve_1()
+        {
+            var count = 0;
+            foreach (var group in _groups)
+            {
+                count += group.SelectMany(x => x.Distinct()).ToList().Distinct().Count();
+            }
+
+            return count.ToString();
+        }
+
+        public override string Solve_2()
+        {
+            var count = 0;
+            foreach (var group in _groups)
+            {
+                count += group.SelectMany(x => x.Distinct()).ToList().GroupBy(letter => letter).Where(letter => letter.Count() == group.Count).Count();
+            }
+
+            return count.ToString();
+        }
+
+    }
+}
