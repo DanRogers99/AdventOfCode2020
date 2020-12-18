5e7e21e4e97b73430261c7fed9423a10ab872908
Dan Rogers <drogers@trakglobal.co.uk>
2020-12-18 16:18:33 +0000

    Day 18

---
+++ AdventOfCode/Day_18.cs
@@ -0,0 +1,112 @@
+﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public class Day_18 : BaseDay
+    {
+        readonly List<string> _file;
+        public Day_18()
+        {
+            _file = File.ReadAllLines(InputFilePath).ToList();
+        }
+
+        public override string Solve_1()
+        {
+            long sum = 0;
+            foreach (var item in _file)
+            {
+                string total = new string(SolveBracket(new String(item.Replace(" ", "")).ToCharList(), false).ToArray());
+                sum += long.Parse(SolveLeftToRight(total, false));
+            }
+            return sum.ToString();
+        }
+
+        public override string Solve_2()
+        {
+            long sum = 0;
+            foreach (var item in _file)
+            {
+                string total = new string(SolveBracket(new String(item.Replace(" ", "")).ToCharList(), true).ToArray());
+                sum += long.Parse(SolveLeftToRight(total, true));
+            }
+            return sum.ToString();
+        }
+
+        private static string SolveLeftToRight(string total, bool additionHigherPrecedenceLevel)
+        {
+            var validChar = new char[] { '+', '-', '/', '*' };
+            var charArray = total.SplitAndKeep(validChar).ToList();
+
+            while (charArray.Count > 1)
+            {
+                int characterIndex = 1;
+                char arithCharacter = charArray[1][0];
+
+                List<CharIndex> priority = new List<CharIndex>();
+
+                if (additionHigherPrecedenceLevel)
+                {
+                    priority.AddRange(charArray.Select((b, i) => b.Equals("+") ? i : -1).Where(i => i != -1).Select(x => new CharIndex { Character = '+', Index = x }));
+                    priority.AddRange(charArray.Select((b, i) => b.Equals("-") ? i : -1).Where(i => i != -1).Select(x => new CharIndex { Character = '-', Index = x }));
+
+                    if (priority.Count != 0)
+                    {
+                        characterIndex = priority.FirstOrDefault().Index;
+                        arithCharacter = priority.FirstOrDefault().Character;
+                    }
+
+                }
+
+                var calculation = ExtentionMethods.Evaluate($"{charArray[characterIndex - 1]} {arithCharacter} {charArray[characterIndex + 1]}");
+
+                for (int i = 0; i < 3; i++)
+                {
+                    charArray.RemoveAt(characterIndex - 1);
+                }
+
+                charArray.Insert(characterIndex - 1, calculation.ToString());
+            }
+            return charArray[0];
+        }
+
+        private List<char> SolveBracket(List<char> bracket, bool additionHigherPrecedenceLevel)
+        {
+            List<CharIndex> brackets = new List<CharIndex>();
+            brackets.AddRange(bracket.Select((b, i) => b.Equals('(') ? i : -1).Where(i => i != -1).Select(x => new CharIndex { Character = '(', Index = x }));
+            brackets.AddRange(bracket.Select((b, i) => b.Equals(')') ? i : -1).Where(i => i != -1).Select(x => new CharIndex { Character = ')', Index = x }));
+            brackets = brackets.OrderBy(x => x.Index).ToList();
+
+            for (int i = 0; i < brackets.Count; i++)
+            {
+                if (brackets[i].Character == '(' && brackets[i + 1].Character == ')')
+                {
+                    var expression = new string(bracket.Skip(brackets[i].Index + 1).Take(brackets[i + 1].Index - brackets[i].Index - 1).ToArray());
+                    bracket.RemoveRange(brackets[i].Index, brackets[i + 1].Index - brackets[i].Index + 1);
+                    bracket.InsertRange(brackets[i].Index, SolveLeftToRight(expression, additionHigherPrecedenceLevel).ToString().ToCharArray());
+                    break;
+                }
+            }
+
+            if (bracket.Contains('('))
+            {
+                SolveBracket(bracket, additionHigherPrecedenceLevel);
+            }
+
+            return bracket;
+        }
+
+        public record CharIndex
+        {
+            public char Character { get; set; }
+            public int Index { get; set; }
+        }
+    }
+}
+
+
--- AdventOfCode/ExtentionMethods.cs
+++ AdventOfCode/ExtentionMethods.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode
@@ -17,6 +19,35 @@ namespace AdventOfCode
         public static List<char> ToCharList(this String str)
         {
             return str.ToCharArray().ToList();
+        }
+
+        public static IEnumerable<string> SplitAndKeep(this string s, char[] delims)
+        {
+            int start = 0, index;
+
+            while ((index = s.IndexOfAny(delims, start)) != -1)
+            {
+                if (index - start > 0)
+                    yield return s[start..index];
+                yield return s.Substring(index, 1);
+                start = index + 1;
+            }
+
+            if (start < s.Length)
+            {
+                yield return s[start..];
+            }
+        }
+
+        public static double Evaluate(string expression)
+        {
+                            return (double)new System.Xml.XPath.XPathDocument
+                (new StringReader("<r/>")).CreateNavigator().Evaluate
+                (string.Format("number({0})", new
+                Regex(@"([\+\-\*])")
+                .Replace(expression, " ${1} ")
+                .Replace("/", " div ")
+                .Replace("%", " mod ")));
         }
     }
 }
